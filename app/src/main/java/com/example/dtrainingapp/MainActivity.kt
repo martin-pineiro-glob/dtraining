6857ec1f8ec44f10d8ca282e349442f3170512bc
Martin Pineiro Vazquez <martin.pineiro@globant.com>
2021-11-24 16:12:00 -0300

    Added appcenter dependencies

--- app/src/main/java/com/example/dtrainingapp/MainActivity.kt
+++ app/src/main/java/com/example/dtrainingapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.dtrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "139050b4-2cae-484d-abe6-66386815dbda",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
